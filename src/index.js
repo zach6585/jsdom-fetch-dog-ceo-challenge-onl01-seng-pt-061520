a6264ce3a7d92e29293eca661aeb8d2146837307
zach6585 <zmines@wesleyan.edu>
2021-01-01 17:45:07 -0500

    commit tree 28c6ca514e84b3a3304e7f984c30d5b5e9d821a3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+document.addEventListener("DOMContentLoaded", function() {
+    fetch(imgUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json) {
+        for (let i = 0; i < json["message"].length; i++) {
+            let div = document.getElementById("dog-image-container");
+            let img = document.createElement("img");
+            img.src = json["message"][i];
+            div.appendChild(img); 
+        }
+    })
+});
